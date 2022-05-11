7c147ae2f472d7c7f8d271352dd50a859bcc085c
jeffersonsnt <jeffersonsnt@live.com>
2022-05-11 13:18:04 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LuizFelipe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hoje eu tenho prova");
+            Console.WriteLine(a);
+
+            string nome = "";
+            Console.Write("Digite Seu Nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine(nome);
+        }
+    }
+}
